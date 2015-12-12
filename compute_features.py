2e4960417f1692c15daaa6dfb2f0257dbe4a7f57
Rishi Rajalingham <rishir@mh17.mit.edu>
2015-12-12 18:54:15 -0500

    clean

--- compute_features.py
+++ compute_features.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import os
 import matplotlib.pyplot as plt
+import scipy.io as io
 
 """ This is a slightly edited version of Shay's OM script."""
 
@@ -13,42 +14,45 @@ STIMPATH = '/mindhive/dicarlolab/u/rishir/stimuli/objectome64s100/'
 STIMPATH_NOBG = '/mindhive/dicarlolab/u/rishir/stimuli/objectome64s100nobg/'
 HVMPATH = '/mindhive/dicarlolab/u/rishir/stimuli/hvm/'
 
-def save_features(features_perlayer, meta, layer, cnn_oi, output_path):
-    features_dict = []
-    features = np.array(features_perlayer[layer])
-    for i,m in enumerate(meta):
-        f_ = {'id':m['id'], 
-            'feature':features[i,:], 
-            'feature_layer':layer, 
-            'model_id':cnn_oi }
-        features_dict.append(f_)
+BATCHSIZE = 40
+MAXNSTIM = 1000000000
 
-    # Save output
-    if os.path.exists(output_path) == False:
-        os.mkdir(output_path)
-    with open(output_path + layer + '.pkl', 'wb') as _f:
-        pk.dump(features_dict, _f)
-    print 'Saved to ' + output_path + layer
+def save_features(features_perlayer, meta, cnn_oi, output_path):
+    layers = features_perlayer.keys()
+    for layer in layers:
+        features_dict = []
+        features = np.array(features_perlayer[layer])
+        for i,m in enumerate(meta[:MAXNSTIM]):
+            f_ = {'id':m['id'], 
+                'feature':features[i], 
+                'feature_layer':layer, 
+                'model_id':cnn_oi }
+            features_dict.append(f_)
 
-def run_model(stimpath=STIMPATH, cnn_oi='VGG_S'):
+        # Save output
+        if os.path.exists(output_path) == False:
+            os.mkdir(output_path)
+        with open(output_path + layer + '.pkl', 'wb') as _f:
+            pk.dump(features_dict, _f)
+        io.savemat(output_path + layer + '.mat', {'features':features})
+        print 'Saved to ' + output_path + layer
+    return
 
-    # Get image files
-    meta = pk.load(open(stimpath + 'metadata.pkl'))
-    filelist = [stimpath + 'images/' + m['id'] + '.png' for m in meta]
-    print("There are "+str(len(filelist))+ " files")
-
-    # Set Model Parameters
+def get_net(stimpath=STIMPATH, cnn_oi='VGG_S'):
     if cnn_oi == 'caffe_reference':
         input_size = 227
         proto_file = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
         model_file = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
         model_input = '/om/user/shayo/caffe/caffe/models/bvlc_reference_caffenet'
+        channel_order = (2,1,0) # the reference model has channels in BGR order instead of RGB
         output_path = stimpath + 'caffe_features/'
+
     elif cnn_oi == 'VGG_S':
         input_size = 224
         proto_file = caffe_root + 'models/VGG_CNN_S/VGG_CNN_S_deploy.prototxt'
         model_file = caffe_root + 'models/VGG_CNN_S/VGG_CNN_S.caffemodel'
         model_input = '/om/user/shayo/caffe/caffe/models/VGG_CNN_S'
+        channel_order = (2,1,0) #???
         output_path = stimpath + 'vgg_features/'
 
     sys.path.insert(0, caffe_root + 'python')
@@ -62,44 +66,54 @@ def run_model(stimpath=STIMPATH, cnn_oi='VGG_S'):
     plt.rcParams['image.cmap']='gray'
     caffe.set_mode_cpu()
     net = caffe.Net(proto_file, model_file, caffe.TEST)
+    net.blobs['data'].reshape(BATCHSIZE,3,input_size,input_size)
 
     # Preprocess input data
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2,0,1))
     transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1)) # mean pixel
     transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
-    transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+    transformer.set_channel_swap('data', channel_order)  
 
-    # Run Model
-    batchsize = 50
-    net.blobs['data'].reshape(batchsize,3,input_size,input_size)
-    indices = np.arange(0,1+len(filelist),batchsize)
-    features_perlayer = {'fc6':[], 'fc7':[], 'fc8':[]}
+    return net, transformer, output_path
 
+
+def run_model(stimpath=STIMPATH, cnn_oi='VGG_S'):
+
+    # Get image files
+    meta = pk.load(open(stimpath + 'metadata.pkl'))
+    filelist = [stimpath + 'images/' + m['id'] + '.png' for m in meta]
+    filelist = filelist[:MAXNSTIM]
+    nstim = len(filelist)
+    print("There are "+str(nstim)+ " files")
+    
+    net, transformer, output_path = get_net(STIMPATH, cnn_oi)
+    compute_layers = {'fc6', 'fc7', 'fc8'}
+    layer_dim = {'fc6':4096, 'fc7':4096, 'fc8':1000}
+    
+    features_perlayer = {}
+    for layer in compute_layers:
+        dim = layer_dim[layer]
+        features_perlayer[layer] = np.zeros([nstim, dim])
+
+    indices = np.arange(0, nstim+1, BATCHSIZE)
     for i in range(0,len(indices)-1):
         print "Processing "+str(indices[i])+" to "+ str(indices[i+1])
         images = filelist[indices[i]:indices[i+1]]
         net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images)
         out = net.forward()
-        features_perlayer['fc6'].extend(net.blobs['fc6'].data)
-        features_perlayer['fc7'].extend(net.blobs['fc7'].data)
-        features_perlayer['fc8'].extend(net.blobs['fc8'].data)
-
+        for layer in compute_layers:
+            outdata = net.blobs[layer].data
+            features_perlayer[layer][indices[i]:indices[i+1], :] = outdata
+        
     return features_perlayer, meta, output_path
 
 
-    # Main
+#     # Main
 cnn_oi = 'caffe_reference'
-features_perlayer, meta, output_path = run_model(stimpath=HVMPATH, cnn_oi=cnn_oi)
-save_features(features_perlayer, meta, 'fc6', cnn_oi, output_path)
-save_features(features_perlayer, meta, 'fc7', cnn_oi, output_path)
-save_features(features_perlayer, meta, 'fc8', cnn_oi, output_path)
+features_perlayer, meta, output_path = run_model(stimpath=STIMPATH, cnn_oi=cnn_oi)
+save_features(features_perlayer, meta, cnn_oi, output_path)
 
 cnn_oi = 'VGG_S'
-features_perlayer, meta, output_path = run_model(stimpath=HVMPATH, cnn_oi=cnn_oi)
-save_features(features_perlayer, meta, 'fc6', cnn_oi, output_path)
-save_features(features_perlayer, meta, 'fc7', cnn_oi, output_path)
-save_features(features_perlayer, meta, 'fc8', cnn_oi, output_path)
-
-
-
+features_perlayer, meta, output_path = run_model(stimpath=STIMPATH, cnn_oi=cnn_oi)
+save_features(features_perlayer, meta, cnn_oi, output_path)
